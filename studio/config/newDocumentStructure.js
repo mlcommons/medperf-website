545d92cae43fe05090378f7749b2d1a900bb67b2
Jenn Scheer <singleportrait@gmail.com>
2022-08-02 15:42:01 -0400

    Customize Sanity settings to disallow deleting or creating new documents

---
+++ studio/config/newDocumentStructure.js
@@ -0,0 +1,4 @@
+import S from '@sanity/base/structure-builder'
+
+// Don't allow anyone to create new Homepage or Settings documents
+export default [];
---
+++ studio/config/resolveDocumentActions.js
@@ -0,0 +1,5 @@
+import defaultResolve from 'part:@sanity/base/document-actions';
+
+export default function resolveDocumentActions(props) {
+  return [...defaultResolve(props).filter((action) => action.name !== 'DeleteAction')]
+}
