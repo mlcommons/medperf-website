71705a402f2f6e499916a05a5908c56676dc0326
Jenn Scheer <singleportrait@gmail.com>
2022-08-06 16:30:45 -0400

    Add arrows between roles in header
    Because of some *strange* opacity issues while positioning, these arrows
    are placed as additional list items after the roles themselves. This
    isn't great from an HTML-perspective, but it makes styling easier, and
    try as I may I could not figure out a way to prevent opacity issues when
    placing them inside the main list items.
    
    When positioning them anywhere outside the main container (e.g. with
    negative positioning or margins), it would cause whatever container it
    was covering to have double-opacity, from what I could tell. Maybe this
    is a paint issue where if something with transparency (even if it's
    small) lays over another element with transparency, the top element
    causes it to be transparent twice?
    
    One thing we could try is to set the stroke color of the arrow to have
    an alpha color, instead of setting the opacity of the container.
    But I'm tired at this moment and maybe I can try that later. It's
    working now though!

--- web/components/Hero.js
+++ web/components/Hero.js
@@ -9,6 +9,7 @@ import BenchmarkIllustration from './illustrations/BenchmarkIllustration';
 import CliniciansIllustration from './illustrations/CliniciansIllustration';
 import ResearchersIllustration from './illustrations/ResearchersIllustration';
 import ResultsIllustration from './illustrations/ResultsIllustration';
+import Arrow from './illustrations/Arrow';
 
 const Hero = ({ hero }) => {
   const { title, description, roles } = hero;
@@ -42,6 +43,16 @@ const Hero = ({ hero }) => {
     animation.goToAndPlay(totalFrames / 5, true);
   };
 
+  // const pauseAnimation = () => {
+  //   if (!animation) return;
+  //   animation.pause();
+  // };
+
+  // const resumeAnimation = () => {
+  //   if (!animation) return;
+  //   animation.play();
+  // };
+
   animation?.setSpeed(0.5);
 
   animation?.addEventListener('complete', () => {
@@ -70,6 +81,12 @@ const Hero = ({ hero }) => {
           <div>
             <p>{description}</p>
             <button type="button" onClick={() => playAnimation()} className="my-4">Play</button>
+            {/*
+            <br />
+            <button type="button" onClick={() => pauseAnimation()} className="my-4">Pause</button>
+            <br />
+            <button type="button" onClick={() => resumeAnimation()} className="my-4">Resume</button>
+            */}
           </div>
           <div>
             <div ref={illustrationContainer} />
@@ -78,42 +95,68 @@ const Hero = ({ hero }) => {
         <br />
         <br />
         <div className="overflow-x-scroll md:overflow-x-auto -mx-4 md:mx-0 pt-4 md:pt-0">
-          <ul className={`rolesAnimation rolesAnimation--${activeIndex} grid grid-cols-4 md:gap-x-12`}>
+          <ul className={`rolesAnimation rolesAnimation--${activeIndex} relative grid grid-cols-4 md:gap-x-12`}>
             {roles.length > 0
-            && roles.map(({ sectionId, name, description: roleDescription }, i) => (
-              <li
-                key={sectionId}
-                className={classNames({
-                  'px-4 md:px-0 transition-opacity grid grid-rows-roles': true,
-                  'opacity-30': activeIndex !== i + 1 && activeIndex !== 0,
-                })}
-              >
-                <div className={classNames({
-                  'scale-80 md:scale-100 mb-4 md:mb-8 h-36 flex items-center justify-center transition-colors': true,
-                  'text-black': activeIndex !== i + 1,
-                  'text-blue': activeIndex === i + 1,
-                })}
+              && roles.map(({ sectionId, name, description: roleDescription }, i) => (
+                <li
+                  key={sectionId}
+                  className={classNames({
+                    'transition-opacity relative px-4 md:px-0 grid grid-rows-roles': true,
+                    'opacity-30': activeIndex !== i + 1 && activeIndex !== 0,
+                  })}
                 >
-                  {sectionId === 'benchmark-committee' && (
-                    <BenchmarkIllustration />
-                  )}
-                  {sectionId === 'researchers' && (
-                    <ResearchersIllustration />
-                  )}
-                  {sectionId === 'clinicians' && (
-                    <CliniciansIllustration />
-                  )}
-                  {sectionId === 'results' && (
-                    <ResultsIllustration />
-                  )}
-                </div>
-                <p className="mb-4 self-center">{i + 1}. {name}</p>
-                <p className="mb-4">{roleDescription}</p>
-                <Link href={`#${sectionId}`} scroll={false} passHref>
-                  <button type="button" className="justify-self-start underline">Read more</button>
-                </Link>
-              </li>
-            ))}
+                  <div className={classNames({
+                    'relative scale-80 md:scale-100 mb-4 md:mb-8 h-36 flex items-center justify-center transition-colors': true,
+                    'text-black': activeIndex !== i + 1,
+                    'text-blue': activeIndex === i + 1,
+                  })}
+                  >
+                    {sectionId === 'benchmark-committee' && (
+                      <BenchmarkIllustration />
+                    )}
+                    {sectionId === 'researchers' && (
+                      <ResearchersIllustration />
+                    )}
+                    {sectionId === 'clinicians' && (
+                      <CliniciansIllustration />
+                    )}
+                    {sectionId === 'results' && (
+                      <ResultsIllustration />
+                    )}
+                  </div>
+                  <p className="mb-4 self-center">
+                    {i + 1}. {name}
+                  </p>
+                  <p className="mb-4">
+                    {roleDescription}
+                  </p>
+                  <Link href={`#${sectionId}`} scroll={false} passHref>
+                    <button type="button" className="justify-self-start underline">Read more</button>
+                  </Link>
+                </li>
+              ))}
+            {/* We couldn't put these above because of weird opacity issues */}
+            <li className={classNames({
+              'absolute scale-80 top-16 left-1/4 -ml-4 md:ml-0 transition-opacity': true,
+              'opacity-30': activeIndex !== 0,
+            })}
+            >
+              <Arrow />
+            </li>
+            <li className={classNames({
+              'absolute scale-80 top-16 left-1/2 -ml-4 md:ml-0 transition-opacity': true,
+              'opacity-30': activeIndex !== 0,
+            })}
+            >
+              <Arrow />
+            </li>
+            <li className={classNames({
+              'absolute scale-80 top-16 left-3/4 -ml-4 md:ml-0 transition-opacity': true,
+              'opacity-30': activeIndex !== 0,
+            })}
+            >
+              <Arrow />
+            </li>
           </ul>
         </div>
       </div>
