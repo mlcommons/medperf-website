15d6b776653366294dda64be4d950b6f6e6946b2
Jenn Scheer <singleportrait@gmail.com>
2022-08-06 13:03:18 -0400

    Add nice loading state and page title to documentation

--- web/pages/documentation.js
+++ web/pages/documentation.js
@@ -1,18 +1,43 @@
+import Head from 'next/head';
+import { useState } from 'react';
 import { RedocStandalone } from 'redoc';
 
-const Documentation = () => (
-  <RedocStandalone
-    specUrl="https://medperf.org/?format=openapi"
-    options={{
-      lazyRendering: false,
-      hideHostname: false,
-      expandResponses: 'all',
-      pathInMiddlePanel: false,
-      nativeScrollbars: false,
-      requiredPropsFirst: false,
-      fetchSchemaWithQuery: true,
-    }}
-  />
-);
+const pageTitle = 'API Documentation | MedPerf';
+
+const Documentation = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <>
+      <Head>
+        <title key="title">{pageTitle}</title>
+        <meta key="og_title" property="og:title" content={pageTitle} />
+      </Head>
+      <RedocStandalone
+        specUrl="https://medperf.org/?format=openapi"
+        options={{
+          lazyRendering: false,
+          hideHostname: false,
+          expandResponses: 'all',
+          pathInMiddlePanel: false,
+          nativeScrollbars: false,
+          requiredPropsFirst: false,
+          fetchSchemaWithQuery: true,
+          hideLoading: true,
+        }}
+        onLoaded={(error) => {
+          if (!error) {
+            console.log('Yay!');
+            setIsLoaded(true);
+          }
+        }}
+      />
+      {!isLoaded && (
+        <div className="w-full h-screen flex items-center justify-center h2">
+          Loading...
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Documentation;
