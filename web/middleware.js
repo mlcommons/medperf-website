34ac73f48314e5b5aa1f4682cdbecc6a0667206d
hasan7n <hasankassim7@hotmail.com>
2023-09-25 15:18:34 +0000

    add redirects

--- web/middleware.js
+++ web/middleware.js
@@ -14,5 +14,15 @@ export const middleware = (request) => {
       return NextResponse.redirect(request.nextUrl.origin);
     }
   }
+  if (request.nextUrl.pathname.startsWith('/callback')) {
+    const params = new URLSearchParams(request.nextUrl.search);
+    if (params.has('error')) {
+      return NextResponse.redirect(request.nextUrl.origin + '/login-failed');
+    }
+    if (params.has('code')) {
+      return NextResponse.redirect(request.nextUrl.origin + '/welcome');
+    }
+    return NextResponse.redirect(request.nextUrl.origin);
+  }
   return NextResponse.next();
 };
